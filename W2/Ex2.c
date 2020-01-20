c1216c4d5a45423ef5d31b1a768df73d1c26c440
Angela Zavaleta <angelazb@Angelas-MacBook-Pro.local>
2020-01-20 11:30:52 -0500

    Week 2 code update

--- W2/Ex2.c
+++ W2/Ex2.c
@@ -7,6 +7,9 @@ int main(){
     char original[1024]="This is the original string!";
     char unoriginal[1024]="And this is another string!";
 
+    // Why does this does not work?!
+    // original = unoriginal;
+
     strcpy(original, unoriginal);
 
     printf("%s\n",original);
--- W2/Ex3.c
+++ W2/Ex3.c
@@ -10,12 +10,18 @@ int main(){
        array_two[i]=i;
     }
 
+   // Why does this does not work?!
+   // array_one = array_two;
+
+   // It looks like I have to do it manually huh...
     for (int i=0; i<5; i++){
        array_one[i]=array_two[i];
     }
 
     for (int i=0; i<10; i++){
        printf("The element %d is: %d\n",i, array_one[i]);
+
+       // Why is it printing weird stuff?!
     }
 
 }
